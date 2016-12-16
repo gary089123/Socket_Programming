5fbc7fc675b69068968e34af7321b78cc59da504
gary <103502022@cc.ncu.edu.tw>
2016-12-16 10:27:48 +0800

    add gui to webserver

--- test1.rb
+++ test1.rb
@@ -5,5 +5,6 @@ client=TCPServer.new(1234)
 while session=client.accept
   request=session.gets
   session.puts request
+  request.gsub(/GET\ \//,"").gsub("")
   session.close
 end
--- webserver.rb
+++ webserver.rb
@@ -1,4 +1,50 @@
 require "socket"
+require "tk" #sudo apt-get install libtcltk-ruby
+
+def gui
+  p_server = proc {server}
+  p_log = proc {viewlog}
+
+  windows = TkRoot.new do
+    title __FILE__.gsub(File.extname(__FILE__),"")
+  end
+  @portText = TkLabel.new(windows) {
+    text "Port:"
+    width 8
+    height 1
+    grid('row'=>0, 'column'=>0, 'columnspan'=>1)
+  }
+  @portField = TkEntry.new(windows) {
+    width 10
+    grid('row'=>0, 'column'=>1, 'columnspan'=>3)
+  }
+  @startstopbutton = TkButton.new(windows){
+    text "Start"
+    width 10
+    grid('row'=>2, 'column'=>0)
+    command p_server
+  }
+  @viewlogbutton = TkButton.new(windows){
+    text "View Log"
+    width 20
+    grid('row'=>2, 'column'=>1)
+    command p_log
+  }
+  @text = Tk::Tile::Notebook.new(windows){
+    width 50
+    height 10
+    grid('row'=>1, 'column'=>0, 'columnspan'=>2)
+  }
+  f1 = TkFrame.new(@text){
+    width 50
+    height 10
+  }
+
+  @text.add f1, :text => 'Status', :state =>'disabled'
+
+  Tk.mainloop
+end
+
 
 
 def contentType(path)
@@ -35,65 +81,86 @@ end
 def startinfo
 
   puts "*************************************************************"
-  puts "#{__FILE__.gsub(File.extname(__FILE__),"")} is starting on http://localhost:2000"
-  puts "Start :  PID = #{Process.pid}  PORT = 2000"
+  puts "#{__FILE__.gsub(File.extname(__FILE__),"")} is starting on http://localhost:#{@portField.value}"
+  puts "Start :  PID = #{Process.pid}  PORT = #{@portField.value}"
   puts "*************************************************************"
 
 end
 
-startinfo
-webserver = TCPServer.new('localhost', 2000)
-base_dir = Dir.new(".")
-logStr=String.new
-while (session = webserver.accept)
+def viewlog
   Thread.start do
-    request=session.gets
-    puts "Connection from #{session.peeraddr[2]} at #{session.peeraddr[3]}"
-    logStr =  "Connection: #{session.peeraddr[2]} (#{session.peeraddr[3]})\n"
-    logStr += "Time      : "+Time.now.localtime.strftime("%Y/%m/%d %H:%M:%S")+"\n"
-    logStr += "Request   : #{request}"
-    method=request.gsub(/\ \/.*/,'').chomp
-    if method=="GET"
-      request_path = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '').chomp
-    elsif method=="POST"
-      request_path = request.gsub(/POST\ \//, '').gsub(/\ HTTP.*/, '').chomp
-    end
+    system("cat ./#{__FILE__.gsub(File.extname(__FILE__),"")}.log")
+  end
+end
 
-    if request_path==""
-      request_path="."
-    end
+def server
+  if @status==false
+    Thread.start do
+      startinfo
+      @webserver = TCPServer.new('localhost', @portField.value.to_i)
+      @status=true
+      @startstopbutton.text="Stop"
+      base_dir = Dir.new(".")
+      logStr=String.new
+      while (session = @webserver.accept)
+        Thread.start do
+          request=session.gets
+          puts "Connection from #{session.peeraddr[2]} at #{session.peeraddr[3]}"
+          logStr =  "Connection: #{session.peeraddr[2]} (#{session.peeraddr[3]})\n"
+          logStr += "Time      : "+Time.now.localtime.strftime("%Y/%m/%d %H:%M:%S")+"\n"
+          logStr += "Request   : #{request}"
+          method=request.gsub(/\ \/.*/,'').chomp
+          if method=="GET"
+            request_path = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '').chomp
+          elsif method=="POST"
+            request_path = request.gsub(/POST\ \//, '').gsub(/\ HTTP.*/, '').chomp
+          end
 
-    if File.directory?(request_path)
-      base_dir = Dir.new(request_path)
-      base_dir.entries.each do |f|
-        if File.basename(__FILE__).gsub(/\..*/,"")==File.basename(f).gsub(/\..*/,"")
-          # do nothing
-        elsif File.directory?(f)
-          session.print "#{f}/\r\n"
-        else
-          session.print "#{f}\r\n"
+          if request_path==""
+            request_path="."
+          end
+
+          if File.directory?(request_path)
+            base_dir = Dir.new(request_path)
+            base_dir.entries.each do |f|
+              if File.basename(__FILE__).gsub(/\..*/,"")==File.basename(f).gsub(/\..*/,"")
+                # do nothing
+              elsif File.directory?(f)
+                session.print "#{f}/\r\n"
+              else
+                session.print "#{f}\r\n"
+              end
+            end
+
+          elsif !File.exist?(request_path)
+            session.print "404 - Resource Cannot be Found"
+            logStr += "Response  : 404 - Resource Cannot be Found\n"
+
+          else
+            contentType=contentType(request_path)
+            session.print "HTTP/1.1 200/OK\r\nContent-type: #{contentType}\r\n\r\n"
+            puts "HTTP/1.1 200/OK  Content-type: #{contentType}"
+            logStr += "Response  : HTTP/1.1 200/OK  Content-type: #{contentType}\n"
+            File.open(request_path) do |f|
+              while (!f.eof?) do
+                buffer = f.read(256)
+                session.write(buffer)
+              end
+            end
+          end
+          log(logStr)
+          session.close
+          puts "session close"
         end
       end
-
-    elsif !File.exist?(request_path)
-      session.print "404 - Resource Cannot be Found"
-      logStr += "Response  : 404 - Resource Cannot be Found\n"
-
-    else
-      contentType=contentType(request_path)
-      session.print "HTTP/1.1 200/OK\r\nContent-type: #{contentType}\r\n\r\n"
-      puts "HTTP/1.1 200/OK  Content-type: #{contentType}"
-      logStr += "Response  : HTTP/1.1 200/OK  Content-type: #{contentType}\n"
-      File.open(request_path) do |f|
-        while (!f.eof?) do
-          buffer = f.read(256)
-          session.write(buffer)
-        end
-      end
     end
 
-    log(logStr)
-    session.close
+  else
+    @webserver.close
+    @status=false
+    @startstopbutton.text="Start"
   end
 
 end
+@status=false
+gui
